8bc9282b1428a41fc2bbd194f7382625ad20a102
ADMIN <alina.velichko1@mail.ru>
2023-01-31 16:03:58 +0300

    from class to methods

--- tests/utils_test.py
+++ tests/utils_test.py
@@ -1,42 +1,44 @@
 import pytest
-from utils import Data
+import utils
 
 
-data = Data("../data/posts.json", "../data/comments.json")
+def test_get_posts_all():
+    assert utils.get_posts_all("./data/posts.json") != [], "JSON-файл с постами не содержит постов!"
 
 
-class TestData:
-    def test_init_type_error(self):
-        with pytest.raises(TypeError):
-            data_new = Data("filename.png", "./data/posts.json")
+def test_get_comments_all():
+    assert utils.get_comments_all("./data/comments.json") != [], "JSON-файл с комментариями не содержит комментариев!"
 
-    def test_get_posts_all(self):
-        assert data.get_posts_all() != [], "JSON-файл с постами не содержит постов!"
 
-    def test_get_comments_all(self):
-        assert data.get_comments_all() != [], "JSON-файл с комментариями не содержит комментариев!"
+def test_get_posts_by_user_value_error():
+    with pytest.raises(ValueError):
+        utils.get_posts_by_user("Alina", "./data/posts.json", "./data/comments.json")
 
-    def test_get_posts_by_user_value_error(self):
-        with pytest.raises(ValueError):
-            data.get_posts_by_user("Alina")
 
-    def test_get_posts_by_user(self):
-        assert data.get_posts_by_user("ralf") == [], "Ошибка в получении поста"
+def test_get_posts_by_user():
+    assert utils.get_posts_by_user("ralf", "./data/posts.json", "./data/comments.json") == [], \
+        "Ошибка в получении поста"
 
-    def test_get_comments_by_post_id_value_error(self):
-        with pytest.raises(ValueError):
-            data.get_comments_by_post_id("Привет")
 
-    def test_get_comments_by_post_id(self):
-        assert data.get_comments_by_post_id(8) == [], "Ошибка в получении комментария поста"
+def test_get_comments_by_post_id_value_error():
+    with pytest.raises(ValueError):
+        utils.get_comments_by_post_id("Привет", "./data/posts.json", "./data/comments.json")
 
-    def test_search_for_posts_value_error(self):
-        with pytest.raises(TypeError):
-            data.search_for_posts(5)
 
-    def test_search_for_posts(self):
-        assert data.search_for_posts("Еда") != [], "Постов с данным включением нет!"
+def test_get_comments_by_post_id():
+    assert utils.get_comments_by_post_id(8, "./data/posts.json", "./data/comments.json") == [], \
+        "Ошибка в получении комментария поста"
 
-    def test_get_post_by_pk(self):
-        with pytest.raises(TypeError):
-            data.get_post_by_pk("Еда")
+
+def test_search_for_posts_value_error():
+    with pytest.raises(TypeError):
+        utils.search_for_posts(5, "./data/posts.json")
+
+
+def test_search_for_posts():
+    assert utils.search_for_posts("Еда", "./data/posts.json") != [], "Постов с данным включением нет!"
+
+
+def test_get_post_by_pk():
+    with pytest.raises(TypeError):
+        utils.get_post_by_pk("Еда", "./data/posts.json")
